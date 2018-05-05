2cd3dc9e9af5f4aa4ed70c678b7df744c5643f3f
skullymax <38359146+skullymax@users.noreply.github.com>
2018-05-05 20:29:16 +0200

    Update gen.js

--- commands/gen.js
+++ commands/gen.js
@@ -3,16 +3,6 @@ function doMagic8BallVoodoo() {
     'adamboerema@gmail.com:acb7311988',
     'raziel.secur@gmail.com:z1x2q3c4',
     'amikaysbokays@aol.com:meow2000',
-    'satch3l@gmail.com:sevamix',
-    'brandonp.pwns@yahoo.com:phung11',
-    'colovos.kevin@gmail.com:as506345',
-    'jackedwardshey@hotmail.co.uk:horseey12',
-    'halliemrizek@gmail.com:kaulitz2',
-    'charlielewisnz@gmail.com:11churchill',
-    'juldeleon19@gmail.com:angel555',
-    'loco.qwer@gmail.com:Kalejdoskop1',
-    'mnieckula@yahoo.com:graalfox87',
-    'krishmuttu9@gmail.com:Krishnam9',
     'austincruz8@gmail.com:jarjarac8'];
 
     return rand[Math.floor(Math.random()*rand.length)];
