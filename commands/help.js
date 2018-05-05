87860660f4a61ce2937ac91a725e004f82e278b2
skullymax <38359146+skullymax@users.noreply.github.com>
2018-05-05 19:09:09 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,7 @@
 const settings = require('../settings.json');
 exports.run = (client, message, params) => {
-  const embed = new RichEmbed()
-  .setColor(0x00AE86)
-  .setTimestamp()
-  .setDescription(`Check commands here: http://eyzalts.us/alts/bot/`)
+  message.channel.send("Check commands here: http://eyzalts.us/alts/bot/");
+
 }
 
 exports.conf = {
